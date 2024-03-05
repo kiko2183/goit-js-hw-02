2621e15a111a0709df8d2816083a2d7ca59ca1a5
kiko2183 <romankulicnij@gmail.com>
2024-03-05 19:16:56 +0200

    123

--- js/task-1.js
+++ js/task-1.js
@@ -1,9 +1,9 @@
 function makeTransaction(quantity, pricePerDroid, customerCredits) {
-  let totalPrice = quantity * pricePerDroid;
+  const totalPrice = quantity * pricePerDroid;
   if (totalPrice <= customerCredits) {
-    console.log(`You ordered ${quantity} droids worth ${totalPrice} credits!`);
+    return `You ordered ${quantity} droids worth ${totalPrice} credits!`;
   } else {
-    console.log("Insufficient funds!");
+    return "Insufficient funds!";
   }
 }
 console.log(makeTransaction(5, 3000, 23000)); // "You ordered 5 droids worth 15000 credits!"
